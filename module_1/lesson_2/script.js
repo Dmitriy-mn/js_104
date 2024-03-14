3d8591d348cb7bf5e4504574071d39340f9b4cba
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2024-03-14 22:15:40 +0200

    lesson_2

---
+++ module_1/lesson_2/script.js
@@ -0,0 +1,25 @@
+/**
+ * Напиши функцію calcBMI(weight, height) яка розраховує та повертає індекс
+ * маси тіла людини. Для цього необхідно розділити вагу на
+ * квадрат висоти.
+ *
+ * Вага та висота будуть спеціально передані як рядки. Не цілі числа можуть
+ * бути задані у вигляді 24.7 або 24,7, тобто як роздільник дробової
+ * частини може бути кома.
+ *
+ * Індекс маси тіла необхідно округлити до однієї цифри після коми.
+ */
+
+
+// function calcBMI(weight, height) {
+//     let numWeight = weight.replace(",", ".");
+//     numWeight = Number.parseFloat(numWeight);
+//     const numHeight = Number.parseFloat(height);
+//     const bmi = numWeight / (numHeight ** 2);
+//     return bmi.toFixed(1);
+// }
+
+// const bmi = calcBMI("88,3", "1.75");
+// console.log(bmi);
+
+
