667b811a128618e4d063f858cc22811e6717eee6
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2024-03-28 22:01:33 +0200

    lesson_6

---
+++ module_3/lesson_6/script.js
@@ -0,0 +1,65 @@
+
+/**
+ * Напиши функцію formatTime(totalMinutes) яка переведе значення
+ * totalMinutes (кількість хвилин) у рядок у форматі годин
+ * та хвилин HH:MM.
+ */
+
+// function formatTime(totalMinutes) {
+//     const hours = Math.floor(totalMinutes / 60);
+//     const minutes = totalMinutes % 60;
+
+//     const doubleHours = String(hours).padStart(2, 0);
+//     const doubleMinutes = String(minutes).padStart(2, 0);
+
+//     return `${doubleHours}:${doubleMinutes}`
+// }
+
+// console.log(formatTime(70))
+// console.log(formatTime(460))
+// console.log(formatTime(1441))
+
+
+
+
+
+
+
+
+
+// Створити функцію яка буде розбивати початковий масив 
+// на потрібну кількість елементів розділюячи на декілька 
+// масивів.Функція приймає 2 параметри
+// 1 - масив значень
+// 2 - потрібну кількість елементіа в масиві
+//  Функція повертає масив масивів
+
+
+               //0  1  2  3  4  5  6  7  8
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// function foo(arr, count) {
+//     const res = [];
+    
+//     for(let i = 0; i < arr.length; i += count) {
+//         const newArr = arr.slice(i, i + count);
+//         console.log(newArr);
+//         res.push(newArr);
+//     }
+//     return res;
+// }
+
+// console.log(foo(data, 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
