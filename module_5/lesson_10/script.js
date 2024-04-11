452247251737c233e477863640daf36c725c499b
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2024-04-11 21:54:07 +0300

    lesson_10

---
+++ module_5/lesson_10/script.js
@@ -0,0 +1,103 @@
+
+
+
+
+
+/**
+ * ---------------------------
+ */
+// const allCars = [
+//   { make: "Honda", model: "CR-V", type: "suv", amount: 14, price: 24045, onSale: true},
+//   { make: "Honda", model: "Accord", type: "sedan", amount: 2, price: 22455, onSale: true },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", amount: 8, price: 24195, onSale: false },
+//   { make: "Mazda", model: "CX-9", type: "suv", amount: 7, price: 31520, onSale: true },
+//   { make: "Toyota", model: "4Runner", type: "suv", amount: 19, price: 34210, onSale: false },
+//   { make: "Toyota", model: "Sequoia", type: "suv", amount: 16, price: 45560, onSale: false },
+//   { make: "Toyota", model: "Tacoma", type: "truck", amount: 4, price: 24320, onSale: true },
+//   { make: "Ford", model: "F-150", type: "truck", amount: 11, price: 27110, onSale: true },
+//   { make: "Ford", model: "Fusion", type: "sedan", amount: 13, price: 22120, onSale: true },
+//   { make: "Ford", model: "Explorer", type: "suv", amount: 6, price: 31660, onSale: false }
+// ];
+
+
+/**
+ * Нехай функція filterByPrice повертає масив автомобілів ціна яких менша ніж
+ * значення параметра threshold.
+ */
+
+// const filterByPrice = (cars, threshold) => {
+//   return cars.filter(item => item.price < threshold);
+// }
+
+// console.table(filterByPrice(allCars, 30000));  
+// console.table(filterByPrice(allCars, 25000));  
+
+
+
+
+
+/**
+ * Рахуємо загальну кількість годин
+ */
+
+// const players = [
+//   { id: "player-1", name: "Mango", timePlayed: 310, online: false },
+//   { id: "player-2", name: "Poly", timePlayed: 470, online: true },
+//   { id: "player-3", name: "Kiwi", timePlayed: 230, online: true },
+//   { id: "player-4", name: "Ajax", timePlayed: 150, online: false },
+//   { id: "player-5", name: "Chelsey", timePlayed: 80, online: true },
+// ];
+                           
+
+// const total = players.reduce((acc, player) => acc + player.timePlayed);
+
+// console.log(total);
+
+
+
+/**
+ * -------------------------------------
+ */
+// const allCars = [
+//   { make: "Honda", model: "CR-V", type: "suv", amount: 14, price: 24045, onSale: true},
+//   { make: "Honda", model: "Accord", type: "sedan", amount: 2, price: 22455, onSale: true },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", amount: 8, price: 24195, onSale: false },
+//   { make: "Mazda", model: "CX-9", type: "suv", amount: 7, price: 31520, onSale: true },
+//   { make: "Toyota", model: "4Runner", type: "suv", amount: 19, price: 34210, onSale: false },
+//   { make: "Toyota", model: "Sequoia", type: "suv", amount: 16, price: 45560, onSale: false },
+//   { make: "Toyota", model: "Tacoma", type: "truck", amount: 4, price: 24320, onSale: true },
+//   { make: "Ford", model: "F-150", type: "truck", amount: 11, price: 27110, onSale: true },
+//   { make: "Ford", model: "Fusion", type: "sedan", amount: 13, price: 22120, onSale: true },
+//   { make: "Ford", model: "Explorer", type: "suv", amount: 6, price: 31660, onSale: false }
+// ];
+
+/**
+ * Нехай функція getAvailableCarNames повертає масив моделей автомобілів,
+ * але тільки тих, які зараз на розпродажі.
+ */
+
+// const getAvailableCarNames = (cars) => {
+//   return cars       
+//     .filter(car => car.onSale)
+//     .map(lalala => lalala.model)
+// }
+
+// console.log(getAvailableCarNames(allCars));
+
+
+
+/**
+ * Нехай функція getSortedCarsOnSale повертає масив автомобілів
+ * на розпродажі (Властивість onSale), відсортованих за зростанням ціни.
+ */
+
+
+// function getSortedCarsOnSale(array) {
+//   return array
+//     .filter(item => !item.onSale) // фільтрую авто на розпродажі
+//     .toSorted((a, b) => a.price - b.price) // сортую за ціною
+// }
+
+// console.table(getSortedCarsOnSale(allCars));
+
+
